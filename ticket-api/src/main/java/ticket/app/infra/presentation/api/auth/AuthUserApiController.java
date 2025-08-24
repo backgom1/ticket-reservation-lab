d1576bf9869769ef3f5fdef99936779a5afd297b
eunseong <dmstjd0214@naver.com>
2025-08-25 00:11:52 +0900

    Feat : "기초 셋팅 작업 및 도메인 모델 설계"

---
+++ ticket-api/src/main/java/ticket/app/infra/presentation/api/auth/AuthUserApiController.java
@@ -0,0 +1,17 @@
+package ticket.app.infra.presentation.api.auth;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 관리자 계정 인증을 담당하는 컨트롤러 클래스
+ * @since 2025-08-25
+ * @author eunsung
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class AuthUserApiController {
+
+}
